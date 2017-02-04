d7520152020b76c30852fd03bd1e9e91101f9e4e
Swift <swift@valter.local>
2017-02-03 22:36:41 -0200

    basico

--- Aula_015_ConceitoGame/GameScene.swift
+++ Aula_015_ConceitoGame/GameScene.swift
@@ -1,89 +1,58 @@
-//
-//  GameScene.swift
-//  Aula_015_ConceitoGame
-//
-//  Created by Swift on 03/02/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
-
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //MARK: Propriedades
+    var backG1 = SKSpriteNode()
+    var backG2 = SKSpriteNode()
+    var player = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        backG1 = self.childNode(withName: "espaco") as! SKSpriteNode
+        backG2 = self.childNode(withName: "espaco2") as! SKSpriteNode
+        player = self.childNode(withName: "player") as! SKSpriteNode
+    
+
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
+        for toque in touches{
+            let local = toque.location(in: self)
+            if player.contains(local) {
+                player.position.y = local.y
+                player.position.x = local.x
+            }
+        }
+
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let velocidade : CGFloat = 7.5
+        backG1.position = CGPoint(x: backG1.position.x - velocidade, y: backG1.position.y)
+        backG2.position = CGPoint(x: backG2.position.x - velocidade, y: backG2.position.y)
+        
+        if backG1.position.x < -backG1.size.width{
+            backG1.position = CGPoint(x: backG2.position.x + backG2.size.width, y: backG1.position.y)
+        }
+        
+        if backG2.position.x < -backG2.size.width{
+            backG2.position = CGPoint(x: backG1.position.x + backG1.size.width, y: backG2.position.y)
+        }
     }
 }
